3c91f3798936ebd1dffd5e370c3f385b0a5e27db
Jaye <735740291@qq.com>
2020-07-14 17:49:29 +0800

    recommend

--- travel/src/pages/home/Home.vue
+++ travel/src/pages/home/Home.vue
@@ -1,9 +1,11 @@
 
 <template>
   <div>
-     <home-header></home-header>
-     <home-swiper></home-swiper>
-     <home-icons></home-icons>
+    <home-header></home-header>
+    <home-swiper></home-swiper>
+    <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-Weekend></home-Weekend>
   </div>
 </template>
 
@@ -11,12 +13,16 @@
 import HomeHeader from './components/Header'
 import HomeSwiper from './components/Swiper'
 import HomeIcons from './components/Icons'
+import HomeRecommend from './components/Recommend'
+import HomeWeekend from './components/Weekend'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
     HomeSwiper,
-    HomeIcons
+    HomeIcons,
+    HomeRecommend,
+    HomeWeekend
   }
 }
 </script>
