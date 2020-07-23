b371e82d68e5a5c2a45a3f4fc43b156fc11ff035
Jaye <735740291@qq.com>
2020-07-23 14:58:28 +0800

    fix 程序问题

--- travel/src/pages/city/components/Alphabet.vue
+++ travel/src/pages/city/components/Alphabet.vue
@@ -53,7 +53,7 @@ export default {
           if (index >= 0 && index < this.letters.length) {
             this.$emit('change', this.letters[index])
           }
-        }, 16)
+        }, 7)
       }
     },
     handleTouchEnd () {
